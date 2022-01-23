7b23c4147326eae266e7f83437cafcae019d23d1
suthakaranr <64707100+suthakaranr@users.noreply.github.com>
2022-01-23 11:03:51 -0800

    Add files via upload

---
+++ R/BetaFunctionsBounds.R
@@ -0,0 +1,235 @@
+## Theorem 3.1
+
+them_3_1 = function(x,y){
+  cond_1 = x/(x+1) + y/(y+1)
+  cond_2 = x*y
+  if(x > 0 && y > 0 && cond_1 <=1 && cond_2 <1){
+    return(c("beta_xy =", beta(x,y), 
+             "lower bound = ", (x+y)*(1 - sqrt(x*y))^2 / (x*y*(x+1)*(y+1))))
+  }
+  else{
+    return("Try x > 0 & y > 0 & x/(x+1) + y/(y+1) <=1 & x*y < 1")
+  }
+}
+
+#them_3_1(1/2,1/2)
+
+
+## Theorem 3.2
+
+
+them_3_2 = function(x,y){
+  cond_1 = x/(x+1) + y/(y+1)
+  z = (1 - x/(x+1) - y/(y+1))/ sqrt(x/((x+1)^2 * (x+2)) + y/((y+1)^2 * (y+2)) )
+  if(cond_1 <=1 && x > 0 && y >0){
+    return(c("beta_xy =", beta(x,y), "lower bound = ", (x+y)/x * z^2 /(y*(z^2 + 1)) ) )
+  }
+  else{
+    return("Try x >0 & y > 0 & x/(x+1) + y/(y+1) < 1")
+  }
+}
+
+#them_3_2(1/2,1/2)
+
+
+## Theorem 3.3
+
+them_3_3 = function(x,y){
+  cond_1 = x/(x+1) + y/(y+1)
+  if(cond_1 <=1 && x > 0 && y >0){
+    return(c("beta_xy =", beta(x,y), "lower bound = ",  (x+y)/x * (1 - x/(x+1) - y/(y+1))  ))
+  }
+  else{
+    return("Try x >0 & y > 0 & x/(x+1) + y/(y+1) <= 1")
+  }
+}
+
+#them_3_3(1/2,1/2)
+
+
+## Theorem 3.4
+
+them_3_4 = function(x,y){
+  cond_1 = x/(x+1) + y/(y+1)
+  cond_2 = x*y
+  s = 0.5 * log(1/(x*y))
+  w_xy = exp(-s) * (1 + s * (x/(x+1)) 
+                    + (exp(s) - 1- s)* x/(x+2) ) * (1 + s * (y/(y+1)) + (exp(s) - 1- s)* y/(y+2) )
+  if(cond_1 <=1 && x > 0 && y >0 && cond_2 <=1){
+    return(c("beta_xy =", beta(x,y), "lower bound = ",  (x+y)/x * (1 - w_xy)  ))
+  }
+  else{
+    return("Try x >0 & y > 0 & x/(x+1) + y/(y+1) <= 1 & x*y < 1")
+  }
+}
+
+#them_3_4(1/2,1/2)
+
+
+## Theorem 3.5
+
+them_3_5 = function(x,y){
+  cond_1 = 1/(x+1) + 1/(y+1)
+  k_xy = (1 - (x/(x+1) + y/(y+1)) )/ sqrt(x/((x+1)^2 * (x+2)) + y/((y+1)^2 * (y+2)) )
+  p_xy = 1/(1 + k_xy^2)
+  if(cond_1 <=1 && x > 0 && y >0){
+    return(c("beta_xy =", beta(x,y), "upper bound = ",  (x+y)/(x*y) * p_xy  ))
+  }
+  else{
+    return("Try x >0 & y > 0 & 1/(x+1) + 1/(y+1) <= 1")
+  }
+}
+
+#them_3_5(2,2)
+
+
+
+## Theorem 3.6
+
+them_3_6 = function(x,y){
+  cond_1 = (x+1)/x
+  k_xy = (1/y - x/(x+1) )/ sqrt(x/((x+1)^2 * (x+2)) + y/((y+1)^2 * (y+2)) )
+  p_xy = 1/(1 + k_xy^2)
+  if(x > 0 && y >cond_1 && cond_1>1){
+    return(c("beta_xy =", beta(x,y), "upper bound = ", 1/x * p_xy  ))
+  }
+  else{
+    return("Try x > 0 and y > (x+1)/x > 1")
+  }
+}
+
+#them_3_6(2,2)
+
+
+
+## Theorem 3.7
+
+them_3_7 = function(x,y){
+  if(x > 0 && y >0 && x <= y){
+    if(x < 1 && x + y <= 1){
+      return(c("beta_xy_a =", beta(x,y), "lower bound = ", (1-x)^(x+y-1) * pi/sin(pi*x)  ))
+    }
+    else if(x < 1 && x + y >=2){
+      return(c("beta_xy_a =", beta(x,y), "lower bound = ", (1-x)^(x+y-1) * pi/sin(pi*x)  ))
+    }
+    else  (x < 1 && x + y > 1 &&  x + y < 2)
+    return(c("beta_xy_b =", beta(x,y), "upper bound = ", (1-x)^(x+y-1) * pi/sin(pi*x)  ))
+  }
+  else{
+    return("Try x > 0 and y> 0")
+  }
+}
+
+#them_3_7(0.25,0.5)
+#them_3_7(0.5,1)
+
+
+
+## Theorem 3.8
+
+them_3_8 = function(x,y){
+  if(x > 1 && y > 1){
+    return(c("beta_xy =", beta(x,y), 
+             "upper bound = ", ((x-1)/(x+y-2))^(x-1) * ((y-1)/(x+y-2))^(y-1)  ))
+  }
+  else{
+    return("Try x > 1 and y > 1")
+  }
+}
+
+#them_3_8(2,2)
+
+
+
+## Theorem 3.10
+
+them_3_10 = function(x,y){
+  u_xy = function(x,y) (1/y) * ( (1/(1+y))^(x-1) 
+                                 +  (0.5 * (x-1) * ( (1+y)^(x-2) -1)) / ( (y-1)^(x-1) * (y+2) ))
+  if(x >= 3 && y > 0 && y < 3){
+    return(c("beta_xy_a =", beta(x,y), "upper bound = ",  u_xy(x,y) ))
+  }
+  else if (x >= 3 && y >= 3)
+    return(c("beta_xy_b =", beta(x,y), "upper bound = ",  min(u_xy(x,y), u_xy(y,x)) ))
+  else{
+    return("Try other values")
+  }
+}
+
+# x >= 3 && y > 0 && y < 3
+#them_3_10(4,2)
+
+# x >= 3 && y >= 3
+#them_3_10(4,3.5)
+
+
+
+## Theorem 3.11
+
+them_3_11 = function(x,y){
+  u_xy = function(x,y) (1/y) * ( (1/(1+y))^(x-1) +  (0.5 * (x-1) * y) / ( (y+1)^(x-1) * (y+2) ))
+  if(x >= 3 && y > 0 && y <3){
+    return(c("beta_xy_a =", beta(x,y), "lower bound = ",  u_xy(x,y) ))
+  }
+  else if (x >= 3 && y >= 3)
+    return(c("beta_xy_b =", beta(x,y), "lwoer bound = ",  max(u_xy(x,y), u_xy(y,x)) ))
+  else{
+    return("Try other values")
+  }
+}
+
+# x >= 3 && y > 0 && y <3
+#them_3_11(4,2)
+
+# x >= 3 && y >= 3
+#them_3_11(5,5)
+
+
+
+## Corollary 2
+
+corollary_2 = function(x,y){
+  u_xy = function(x,y) (1/y) * x^x * (1/(x+y))^(x+y) * (y+1)^(y+1)
+  if( x >=1 && y>=1 && x<=y ){
+    return(c("boud_a =", beta(x,y), "upper bound = ", min(u_xy(x,y), u_xy(y,x)) ))
+  }
+  else{
+    return("Try x >=1 && y>=1 && x<=y ")
+  }
+}
+
+#corollary_2(1, 1.5)
+
+
+
+
+## Theorem 3.14
+
+them_3_14 = function(x,y){
+  if(x > 0 && y > 0 ){
+    return(c("beta_xy =", beta(x,y), "lower bound = ", (x/(x+y))^x * (exp(-x)/sqrt(x))*sqrt(2*pi)  ))
+  }
+  else{
+    return("Try x > 0 && y > 0")
+  }
+}
+
+#them_3_14(1,2)
+
+
+
+
+## Theorem 3.15
+
+them_3_15 = function(x,y){
+  if(x > 0 && y > 0 &&  x <=y && x< Inf && y < Inf ){
+    return(c("beta_xy =", beta(x,y), 
+             "lower bound = ", (x^x * y^y) /(x + y)^(x + y) * sqrt(2*pi/x)  ))
+  }
+  else{
+    return("Try x > 0 && y > 0")
+  }
+}
+
+#them_3_15(1,2)
+
---
+++ R/BetaFunctionsRatiosBounds.R
@@ -0,0 +1,104 @@
+## Theorem 3.9
+
+them_3_9 = function(x1,x2){
+  if(x1 > 0 && x2 >0 ){
+    if( x1 <= x2 ){
+      return(c("boud_a =", beta(x2,x2)/beta(x1,x1), "upper bound = ", (1/4)^(x2-x1)  ))
+    }
+    else ( x1 > x2 )
+    return(c("boud_b =", beta(x2,x2)/beta(x1,x1), "lower bound = ", (1/4)^(x2-x1)  ))
+  }
+  else{
+    return("Try x1 >0 and x2 > 0")
+  }
+}
+
+
+# x1 <= x2
+#them_3_9(0.5,0.75)
+
+# x1 > x2
+#them_3_9(1.5,1) 
+
+
+## Theorem 3.12
+
+them_3_12 = function(y, x1,x2){
+  if(y > 0 && x1 > 0 && x1 <= x2 && x2 < Inf ){
+    return(c("beta_a =", beta(x2,y)/beta(x1,y),
+             "upper bound = ", x2^x2 *(1/x1)^x1 * (1/(x2 + y))^(x2 + y) * (x1 + y)^(x1 + y)  ))
+  }
+  else{
+    return("Try y > 0,  x1 > 0 and x1 <= x2 < Inf")
+  }
+}
+
+
+## Corollary 1
+
+corollary_ratio_1 = function(x1,x2, y1, y2){
+  g_abc = function(a, b, c) b^b * (1/a)^a * (1/(b+c))^(b+c) * (a+c)^(a+c)
+  if( y1 > 0 && y1 <= y2 && y2 < Inf  && x1 > 0 && x1 <= x2 && x2 < Inf ){
+    return(c("beta_xy =", beta(x2,y2)/beta(x1,y1), 
+             "upper bound = ", g_abc(x1, x2, y2)* g_abc(y1, y2, x1)  ))
+  }
+  else{
+    return("Try y1 > 0 and y1 <= y2 < Inf,  x1 > 0 and x1 <= x2 < Inf")
+  }
+}
+
+#corollary_ratio_1(1, 1.5,2, 3)
+
+
+
+
+## Theorem 3.13
+
+
+them_3_13 = function(y, x1,x2){
+  if(y > 0 && x1 > 0 && x1 <= x2 && x2 < Inf ){
+    return(c("beta_a =", beta(x2,y)/beta(x1,y), "upper bound = ", ((x1+y)/(x2+y))^y  ))
+  }
+  else{
+    return("Try y > 0 && x1 > 0 && x1 <= x2 && x2 < Inf")
+  }
+}
+
+
+# y > 0 && x1 > 0 && x1 <= x2 && x2 < Inf 
+#them_3_13(1, 1.5,2)
+
+
+
+## Theorem 3.16
+
+them_3_16 = function(y, x1,x2){
+  if(y > 0 && x1 > 0 && x1 < x2 && x2 < Inf ){
+    return(c("beta_a =", beta(x2,y)/beta(x1,y), 
+             "lower bound = ", ((x1*(x2+y))/(x2*(x1+y)))* x2^x2 * (1/x1)^x1 * (1/(x2 +y))^(x2 +y) *(x1 +y)^(x1+y)  ))
+  }
+  else{
+    return("Try y > 0 && x1 > 0 && x1 < x2 && x2 < Inf")
+  }
+}
+
+#them_3_16(1, 1.5,2)
+
+
+
+## Theorem 3.17
+
+them_3_17 = function(y, x1,x2){
+  c = 1  +  (y*log( (x2+y)/(x1 + y) ))/(x1 - x2)
+  a1 = log(c) - 1
+  a2 = 1/c
+  if(y > 0 && x1 > 0 && x1 < x2 && x2 < Inf ){
+    return(c("beta_a =", beta(x2,y)/beta(x1,y),
+             "upper bound = ", exp((a1 + a2)*(x2 - x1)) * ( (x1 + y)/(x2 + y) )^(a2 * y)  ))
+  }
+  else{
+    return("Try y > 0 && x1 > 0 && x1 < x2 && x2 < Inf")
+  }
+}
+
+#them_3_17(1, 1.5,2)
